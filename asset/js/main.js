ee831263512a26a791460b3a85f7ffc001a89253
domingo79 <andria140779@yahoo.it>
2021-05-04 20:04:55 +0200

    html

--- asset/js/main.js
+++ asset/js/main.js
@@ -60,15 +60,15 @@ document.querySelector('button').addEventListener('click', function () {
 
     if (discount.includes(controlloCP) && controlloCP == 'cp10') {
         totalBurger -= totalBurger * 0.1;
-        console.log('hai uno sconto 10 ' + totalBurger);
+        //console.log('hai uno sconto 10 ' + totalBurger);
     } else if (discount.includes(controlloCP) && controlloCP == 'cp15') {
         totalBurger -= totalBurger * 0.15;
-        console.log('hai uno sconto 15 ' + totalBurger);
+        //console.log('hai uno sconto 15 ' + totalBurger);
     } else if (discount.includes(controlloCP) && controlloCP == 'cp20') {
         totalBurger -= totalBurger * 0.20;
-        console.log('hai uno sconto 20 ' + totalBurger);
+        //console.log('hai uno sconto 20 ' + totalBurger);
     } else {
-        console.log('prezzo pieno');
+        //console.log('prezzo pieno');
     }
 
     document.getElementById('price').innerHTML = '€ ' + totalBurger;
