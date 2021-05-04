5f31ee71bab0736151efa8c8e640483a2ce6f75e
domingo79 <andria140779@yahoo.it>
2021-05-04 19:37:40 +0200

    logica3

--- asset/js/main.js
+++ asset/js/main.js
@@ -37,7 +37,7 @@ rendeInput(ingredienti, ingredientiElements);
 document.querySelector('button').addEventListener('click', function () {
 
     var burgerBasic = 3.99;
-    console.log(burgerBasic);
+    //console.log(burgerBasic);
 
     var checkIngredienti = document.querySelectorAll("input[type='checkbox']");
     //console.log(checkIngredienti);
@@ -50,9 +50,29 @@ document.querySelector('button').addEventListener('click', function () {
             sumIngredients += Number(element.getAttribute('data-price'))
         }
     }
-    console.log(sumIngredients);
+    //console.log(sumIngredients);
 
-    var totalBurger = burgerBasic + sumIngredients;
+    var totalBurger = (burgerBasic + sumIngredients);
+
+    var discount = ['cp10', 'cp15', 'cp20'];
+
+    var controlloCP = document.querySelector('#discount').value;
+
+    if (discount.includes(controlloCP) && controlloCP == 'cp10') {
+        totalBurger -= totalBurger * 0.1;
+        console.log('hai uno sconto 10 ' + totalBurger);
+    } else if (discount.includes(controlloCP) && controlloCP == 'cp15') {
+        totalBurger -= totalBurger * 0.15;
+        console.log('hai uno sconto 15 ' + totalBurger);
+    } else if (discount.includes(controlloCP) && controlloCP == 'cp20') {
+        totalBurger -= totalBurger * 0.20;
+        console.log('hai uno sconto 20 ' + totalBurger);
+    } else {
+        console.log('prezzo pieno');
+    }
 
     document.getElementById('price').innerHTML = '€ ' + totalBurger;
 })
+
+
+
